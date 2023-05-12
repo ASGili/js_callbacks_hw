bb6fa08a2c4c9551295dbd51a3931ce8556d5efb
Aric Gilinsky <aric.gilinsky@gmail.com>
2023-05-12 13:18:58 +0100

    all tests passed so must be working

--- 03_hw_advanced/hw_advanced_start/isogram_finder/isogram_finder.js
+++ 03_hw_advanced/hw_advanced_start/isogram_finder/isogram_finder.js
@@ -1,9 +1,15 @@
 const IsogramFinder = function (word) {
-
-}
+    this.word = word.toLowerCase().split('')
+};
 
 IsogramFinder.prototype.isIsogram = function () {
-
-}
+    const resultBoolean = this.word.every((letterInWord)=>
+        {if ((this.word.filter((letter)=>letter === letterInWord).length-1)===0){
+        return true
+    };
+});
+    console.log(resultBoolean)
+    return resultBoolean
+};
 
 module.exports = IsogramFinder;
