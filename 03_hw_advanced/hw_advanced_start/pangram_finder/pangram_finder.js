5bcf57b8deb45bd0a0d9fb22a3467313d91c88e1
Aric Gilinsky <aric.gilinsky@gmail.com>
2023-05-12 12:46:39 +0100

    did pangram finder

--- 03_hw_advanced/hw_advanced_start/pangram_finder/pangram_finder.js
+++ 03_hw_advanced/hw_advanced_start/pangram_finder/pangram_finder.js
@@ -1,9 +1,12 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
-}
+  this.phrase = phrase.toLowerCase().split('');
+};
 
 PangramFinder.prototype.isPangram = function () {
-
-}
+  const result = this.alphabet.every((letterInAlphabet) => this.phrase.includes(letterInAlphabet));
+  return result
+};
 
 module.exports = PangramFinder;
+g
