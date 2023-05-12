83cbf09a71d18ee90365d32af0ba7f8bd44d2a86
Aric Gilinsky <aric.gilinsky@gmail.com>
2023-05-12 13:56:34 +0100

    a better working halfway through

--- 03_hw_advanced/hw_advanced_start/anagram_finder/anagram_finder.js
+++ 03_hw_advanced/hw_advanced_start/anagram_finder/anagram_finder.js
@@ -6,11 +6,12 @@ AnagramFinder.prototype.findAnagrams = function (otherWords) {
     const result = otherWords.filter((wordInArray)=>wordInArray.split('').every((letterInArray)=>this.word.includes(letterInArray)))
     
     console.log(result)
-    let length1 = (result)[0].length
-    let length2 = this.word.length
-    console.log(length1)
-    console.log(length2)
+    return result;
+    // let length1 = result[0].length
+    // let length2 = this.word.length
+    // console.log(length1)
+    // console.log(length2)
 
-    if (length1 === length2) {return result};
+    // if (length1 === length2) {return result};
 }
 module.exports = AnagramFinder;
