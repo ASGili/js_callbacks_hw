9b680870f5127b1f28007b9b50d04600c169b7c0
Aric Gilinsky <aric.gilinsky@gmail.com>
2023-05-14 10:12:11 +0100

    intermediate exercise done

--- 03_hw_advanced/hw_intermediate_start/hw_traveller_start/models/traveller.js
+++ 03_hw_advanced/hw_intermediate_start/hw_traveller_start/models/traveller.js
@@ -3,28 +3,42 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  let startLocations = this.journeys.map((journey)=>{return journey.startLocation})
+  return startLocations
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  let endLocations = this.journeys.map((journey)=>{return journey.endLocation})
+  return endLocations
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  let journeysByTransport = this.journeys.filter((journey)=>{return journey.transport === transport})
+  return journeysByTransport
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  let journeysByDistance = this.journeys.filter((journey)=>{return journey.distance > minDistance})
+  return journeysByDistance
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  let totalDistance = this.journeys.map((journey)=>journey.distance).reduce((accumulator,currentValue)=> { return accumulator 
+    += currentValue});
+  return totalDistance
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
+let allTransports = this.journeys.map(journey => journey.transport)
+const uniqueTransports = new Set(allTransports)
+let uniqueTransportsArray = []
 
+for (element of uniqueTransports){
+  uniqueTransportsArray.push(element)
+}
+return uniqueTransportsArray
 };
+
 
 
 module.exports = Traveller;
