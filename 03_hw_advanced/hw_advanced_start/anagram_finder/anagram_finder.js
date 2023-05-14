5c19fca715946121dc852d9beb9763f8f27ceba6
Aric Gilinsky <aric.gilinsky@gmail.com>
2023-05-14 09:32:39 +0100

    finished all extension tests

--- 03_hw_advanced/hw_advanced_start/anagram_finder/anagram_finder.js
+++ 03_hw_advanced/hw_advanced_start/anagram_finder/anagram_finder.js
@@ -3,21 +3,20 @@ this.word = word.toLowerCase().split('')
 };
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-    const result = otherWords.filter((wordInArray)=>wordInArray.toLowerCase().split('').every((letterInArray)=>this.word.includes(letterInArray)))
+    const result = otherWords.filter((wordInArray)=>wordInArray.toLowerCase().split('').every((letterInArray)=>this.word.includes(letterInArray)));
     
     if (result.length == 0) {
-       result.push("a")} //using a one letter string as one letter can never be an anagram of anything else.
+       result.push("p")}; //using a one letter string as placeholder for empty arrays as one letter can never be an anagram of anything else.
 
-    let length1 = result[0].length
-    let length2 = this.word.length
-    // console.log(length1)
-    // console.log(length2)
 
-    if (length1 === length2) {return result}
+    let length1 = result[0].length;
+    let length2 = this.word.length;
+    
+    if (this.word.join('') === result[0]) {return []}
+    else if (length1 === length2) {return result}
     else {return []};
 
-    console.log(result)
-    return result;
+  
 
 }
 
